938f10dbc44d1e92623d005ec576cddb0c274d70
rakshithShetty <rakshith.rs@gmail.com>
2015-11-08 12:53:02 +0200

    Some fixes in preproc

--- dataio/preprocData.py
+++ dataio/preprocData.py
@@ -1,10 +1,11 @@
+import pickle
 from datetime import datetime
-
 import numpy as np
 import pandas as pd
 
 # def init(location):
 location = 'data/trip_data_2.csv'
+out_file = 'data/srtPkupClean.p'
 
 dtypes = {'medallion': str,
           'pickup_datetime': str,
@@ -28,18 +29,23 @@ for tx in df.medallion.unique():
     taxiIds[tx] = i
     i = i + 1
 
-srtPkupData = np.zeros((len(df), 5))
+srtPkupData = np.zeros((len(df),7))
 srtPkupData[:, 0] = np.asarray(map(taxiIds.get, df.medallion.as_matrix()[df.pickup_datetime.order().index]))
 srtPkupData[:, 1] = df.pickup_latitude.as_matrix()[df.pickup_datetime.order().index]
 srtPkupData[:, 2] = df.pickup_longitude.as_matrix()[df.pickup_datetime.order().index]
-srtPkupData[:, 3] = [(df.pickup_datetime[i].to_datetime() - datetime(2013, 2, 1)).total_seconds() for i in
-                     df.pickup_datetime.order().index]
-srtPkupData[:, 4] = 1
+srtPkupData[:,3] = (df.pickup_datetime.order().values - np.datetime64('1970-01-01'))/np.timedelta64(1,'s')
+srtPkupData[:,4] =  df.dropoff_latitude.as_matrix()[ df.pickup_datetime.order().index] 
+srtPkupData[:,5] =  df.dropoff_longitude.as_matrix()[ df.pickup_datetime.order().index] 
+srtPkupData[:,6] = (df.pickup_datetime.order().values - np.datetime64('1970-01-01'))/np.timedelta64(1,'s')
+srtPkupData = srtPkupData[(srtPkupData[:,6] - srtPkupData[:,3]) > 50,:]
+vld_data = np.where((abs(srtPkupData[:,2]+73)<10) * (abs(srtPkupData[:,1]-40)<5) * (abs(srtPkupData[:,4]-40)<5) * (abs(srtPkupData[:,5]+73)<10))
+srtPkupData = srtPkupData[vld_data,:]
 
-srtDrpData = np.zeros((len(df), 5))
-srtDrpData[:, 0] = np.asarray(map(taxiIds.get, df.medallion.as_matrix()[df.dropoff_datetime.order().index]))
-srtDrpData[:, 1] = df.dropoff_latitude.as_matrix()[df.dropoff_datetime.order().index]
-srtDrpData[:, 2] = df.dropoff_longitude.as_matrix()[df.dropoff_datetime.order().index]
-srtDrpData[:, 3] = [(df.dropoff_datetime[i].to_datetime() - datetime(2013, 2, 1)).total_seconds() for i in
-                    df.dropoff_datetime.order().index]
-srtDrpData[:, 4] = 0
+pickle.dump(srtPkupData,open(out_file,'wb'))
+#srtDrpData = np.zeros((len(df),5))
+#srtDrpData[:,0] = np.asarray(map(taxiIds.get,df.medallion.as_matrix()[df.dropoff_datetime.order().index]))
+#srtDrpData[:,1] = df.dropoff_latitude.as_matrix()[df.dropoff_datetime.order().index]
+#srtDrpData[:,2] = df.dropoff_longitude.as_matrix()[df.dropoff_datetime.order().index]
+#srtDrpData[:,3] = [(df.dropoff_datetime[i].to_datetime() - datetime(2013,02,1)).total_seconds() for i in df.dropoff_datetime.order().index]
+#srtDrpData[:,4] = 0
+#
